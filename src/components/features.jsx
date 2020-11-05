3cbcc9e1517956aa7fec12bee704cf9ae0e7ccaf
rajeev <rajeev.rawat.16002@iitgoa.ac.in>
2020-11-05 09:13:11 +0530

    first commit

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -6,12 +6,12 @@ export class features extends Component {
       <div id="features" className="text-center">
         <div className="container">
           <div className="col-md-10 col-md-offset-1 section-title">
-            <h2>Features</h2>
+            <h2>How ESI can help you</h2>
           </div>
           <div className="row">
             {this.props.data
               ? this.props.data.map((d,i) => (
-                  <div  key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+                  <div  key={`${d.title}-${i}`} className="col-xs-6 col-md-4">
                     {" "}
                     <i className={d.icon}></i>
                     <h3>{d.title}</h3>
