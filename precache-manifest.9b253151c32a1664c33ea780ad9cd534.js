f24b16348d5f8ee40769ba99f70a3b3656143eb3
Rajiv Rawat <rajivking25@gmail.com>
2020-11-19 11:27:53 +0530

    Updates

--- precache-manifest.9b253151c32a1664c33ea780ad9cd534.js
+++ precache-manifest.9b253151c32a1664c33ea780ad9cd534.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3196c926cba964645bedf7524d5820fb",
+    "revision": "c8967455310ff137fd86334d752f65a4",
     "url": "/esiServices/index.html"
   },
   {
-    "revision": "c1f2e3ecb57fda8b7e61",
+    "revision": "f0eb73d5434a9ae40e99",
     "url": "/esiServices/static/css/main.8f5e0738.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/esiServices/static/js/2.aff0f010.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c1f2e3ecb57fda8b7e61",
-    "url": "/esiServices/static/js/main.3a0288b9.chunk.js"
+    "revision": "f0eb73d5434a9ae40e99",
+    "url": "/esiServices/static/js/main.6d7d413b.chunk.js"
   },
   {
     "revision": "b592445b8649d60b112e",
